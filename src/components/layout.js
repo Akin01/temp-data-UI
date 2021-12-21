42f563489b4a87fb8019c6657b04422915f69f79
Ainul Yaqin <Akinpasha82@gmail.com>
2021-12-21 10:58:23 +0800

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,9 @@
 const Layout = ({ children }) => {
-    return (
-        <div className='bg-gray-500 p-10 flex-col justify-center items-center  min-h-screen'>
-            {children}
-      </div>
-    );
-}
+  return (
+    <div className='bg-gray-500 p-10 flex-col justify-center items-center  min-h-screen'>
+      {children}
+    </div>
+  );
+};
 
 export default Layout;
